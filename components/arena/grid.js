e766f516765454ec77e9dc9ebe4360f0f327cde3
NairoD34 <peiro.dorian@gmail.com>
2025-01-17 14:47:42 +0100

    Ajustement de la position des inputs et ajout du console.log pour le background

--- components/arena/grid.js
+++ components/arena/grid.js
@@ -3,32 +3,37 @@ import React, { useState } from 'react';
 import styles from './grid.module.css'
 
 export default function Grid({background}){
+    console.log(background);
     const [x, setX] = useState(8);
     const [y, setY] = useState(8);
     return (
-        <>
-        <div className={styles.xyinputs}>
-            <input type="number" value={x} onChange={(e) => setX(parseInt(e.target.value))}/>
-            <input type="number" value={y} onChange={(e) => setY(parseInt(e.target.value))}/>
-        </div>
-        <div className={styles.contenant}>
-        <table className={styles.table} style={{
-            backgroundImage: `url(${background})`,
-            backgroundSize: "cover"
-            }}>
-            <tbody>
-                {[...Array(y)].map((_, rowIndex) => (
-                    <tr className={styles.tr} key={rowIndex}>
-                        {[...Array(x)].map((_, colIndex) => (
-                            <td className={styles.td} key={`${rowIndex}-${colIndex}`">
-
-                            </td>
+        <div className={styles.gridContainer}>
+            <div className={styles.contenant}>
+                <table className={styles.table} style={{
+                    backgroundImage: `url(${background})`,
+                    backgroundSize: "cover"
+                }}>
+                    <tbody>
+                        {[...Array(y)].map((_, rowIndex) => (
+                            <tr className={styles.tr} key={rowIndex}>
+                                {[...Array(x)].map((_, colIndex) => (
+                                    <td className={styles.td} key={`${rowIndex}-${colIndex}`}/>
+                                ))}
+                            </tr>
                         ))}
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+                    </tbody>
+                </table>
+            </div>
+            <div className={styles.xyinputs}>
+                <div>
+                    <label>Largeur:</label>
+                    <input type="number" value={x} min="4" max="10" onChange={(e) => setX(parseInt(e.target.value))}/>
+                </div>
+                <div>
+                    <label>Hauteur:</label>
+                    <input type="number" value={y} min="4" max="10" onChange={(e) => setY(parseInt(e.target.value))}/>
+                </div>
+            </div>
         </div>
-        </>
-    );
+    )
 }
