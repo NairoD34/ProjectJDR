5e12189bb13d751cb2b712bcbae94baaeb01b1e0
NairoD34 <peiro.dorian@gmail.com>
2025-01-17 14:19:23 +0100

    Correction des fautes d'orthographe et ajout de commentaires

--- app/arena/page.js
+++ app/arena/page.js
@@ -1,10 +1,16 @@
+/**
+ * Page de l'arène de combat
+ * Cette page permet d'afficher et gérer la grille de combat pour les parties de JDR
+ */
+
 import Grid from "@/components/arena/grid";
-import BG from "@/assets/icons/western.png"
+import BG from "@/assets/icons/western.png"  // Image de fond pour le thème western
 import Image from "next/image";
 
 export default function ArenaPage(options) {
     return (
         <>
+            {/* Grille de combat avec dimensions 11x11 */}
             <Grid x={11} y={11} background={""}/>
         </>
     );
